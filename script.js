22770675f070f8dcc5b67842bdc742fffaa21fef
Geovar <geoffroy.varlez@gmail.com>
2019-09-11 16:43:19 +0200

    last commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-	var width = 800,
-		height = 480;
+	var width = 1024,
+		height = 748;
 
 	var game = new Phaser.Game(
 		width, height, Phaser.CANVAS, '',
@@ -27,28 +27,31 @@ window.onload = function() {
 		// set the scale mode to cover the entire screen
 
 		// this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		// this.scale.pageAlignVertically = true;
-		// this.scale.pageAlignHorizontally = true;
+		this.scale.pageAlignVertically = true;
+		this.scale.pageAlignHorizontally = true;
 
 		// set the background color of the stage
-		game.stage.backgroundColor = "#ccc";
+		game.stage.backgroundColor = "black";
 
 		// start the Phaser arcade physics engine
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 
 		// create seeker sprite
-		sprSeeker = game.add.sprite(game.world.centerX, game.world.centerY, 'imgSeeker');
-		sprSeeker.anchor.setTo(0.5, 0.5);
-		game.physics.enable(sprSeeker, Phaser.Physics.ARCADE);
+		sprSeeker = new Seeker(game, game.world.centerX, game.world.centerY);
 
-		sprSeeker.MAX_SPEED = 240;
-		sprSeeker.MAX_STEER = 6;
-		sprSeeker.MAX_SPEED_SQ = sprSeeker.MAX_SPEED * sprSeeker.MAX_SPEED;
-		sprSeeker.MAX_STEER_SQ = sprSeeker.MAX_STEER * sprSeeker.MAX_STEER;
+		// sprSeeker = game.add.sprite(game.world.centerX, game.world.centerY, 'imgSeeker');
+		// sprSeeker.anchor.setTo(0.5, 0.5);
+		// game.physics.enable(sprSeeker, Phaser.Physics.ARCADE);
 
-		// create target sprite
+		// sprSeeker.MAX_SPEED = 240;
+		// sprSeeker.MAX_STEER = 6;
+		// sprSeeker.MAX_SPEED_SQ = sprSeeker.MAX_SPEED * sprSeeker.MAX_SPEED;
+		// sprSeeker.MAX_STEER_SQ = sprSeeker.MAX_STEER * sprSeeker.MAX_STEER;
+
+		// // create target sprite
 		sprTarget = game.add.sprite(game.input.x, game.input.y, 'imgTarget');
 		sprTarget.anchor.setTo(0.5, 0.5);
+
 	}
 
 	/**
@@ -59,60 +62,6 @@ window.onload = function() {
 		sprTarget.position.setTo(game.input.x, game.input.y);
 
 		// update seeker to move toward the target
-		seek(sprSeeker, sprTarget);
-	}
-
-	/**
-	 * Updates vehicle velocity so that it moves toward the target.
-	 */
-	function seek(pVehicle, pTarget){
-		
-		var vecDesired;
-		var pVehicle;
-		var pTarget;
-		var vecSteering;
-		var vecCurrent = pVehicle.body.velocity;
-		var forceMax = pVehicle.MAX_STEER;
-		var speedMax = pVehicle.MAX_SPEED;
-		var newVelocity;
-		var scale;
-
-		// 1. vector(desired velocity) = (target position) - (vehicle position)
-		
-		vecDesired = Phaser.Point.subtract(pTarget, pVehicle);
-
-		// 2. normalize vector(desired velocity)
-		
-		vecDesired.normalize();
-
-		// 3. scale vector(desired velocity) to maximum speed
-
-		scale = vecDesired.multiply(speedMax, speedMax);
-
-		// 4. vector(steering force) = vector(desired velocity) - vector(current velocity)
-		
-		vecSteering = Phaser.Point.subtract(vecDesired, vecCurrent);
-
-		// 5. limit the magnitude of vector(steering force) to maximum force
-		
-		if(vecSteering.getMagnitudeSq() > forceMax * forceMax ){
-
-			vecSteering.setMagnitude(forceMax);
-		}
-
-		// 6. vector(new velocity) = vector(current velocity) + vector(steering force)
-		
-		newVelocity = vecCurrent.add(vecSteering.x, vecSteering.y);
-
-		// 7. limit the magnitude of vector(new velocity) to maximum speed
-		
-		if(vecCurrent.getMagnitudeSq() > scale){
-
-			vecCurrent.setMagnitude(speedMax);
-		}
-
-		// 8. update vehicle rotation according to the angle of the vehicle velocity
-
-		pVehicle.rotation = vecReference.angle(vecCurrent);
+		sprSeeker.seek(sprTarget);
 	}
 }
