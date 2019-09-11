92ab6735ff9a3fc7d2b9fdd4906899df3b37b0e4
kital-dwwp <kital82@hotmail.fr>
2019-09-11 11:37:47 +0200

    first-commit

---
+++ Seeker.js
@@ -0,0 +1,17 @@
+class Seeker extends phaser.spirit{
+ 
+ let vec_ref = new Spirit;     
+    constructor (game,posX,posY)
+    {
+        super(game,posX,posY,imgSeeker)
+    }
+
+const sprSeeker.MAX_SPEED = 240;
+const sprSeeker.MAX_STEER = 6;
+
+const sprSeeker.MAX_SPEED_SQ = sprSeeker.MAX_SPEED * sprSeeker.MAX_SPEED;
+const sprSeeker.MAX_STEER_SQ = sprSeeker.MAX_STEER * sprSeeker.MAX_STEER;
+
+
+
+}
